5e2d944ee0d3900a535362abe87a742615cc4f80
cecimendesop <cecilia.mopaulo@gmail.com>
2025-03-28 08:55:59 -0300

    uwwr6ue5

--- ex4.py
+++ ex4.py
@@ -11,19 +11,23 @@ def main(page: ft.Page):
 
     # funções
     def verifica_idade(e):
-        
-        data_atual = datetime.now().date()
-        txt_resultado.value = f"Data de nascimento: {data_atual} - {input_nascimento}"
-        idade = {data_atual} - {input_nascimento}
+        try:
+            txt_resultado.value = input_nascimento.value
+            data_nascimento = datetime.strptime(input_nascimento.value, "%d/%m/%Y")
+            data_atual = datetime.now()
+            idade = data_atual.year - data_nascimento.year
+            if datetime.today().month < data_nascimento.month:
+                idade = idade - 1
+            if int(idade) >= 18:
+                txt_resultado.value = f'Maior de idade'
+            else:
+                txt_resultado.value = f'Menor de idade'
+            page.update()
+        except ValueError:
+            return 'Insira atributos válidos'
 
-        if input_nascimento >= 18:
-            print("Ele tem" {idade}"anos, o que faz dele maior de idade")
-        elif input_nascimento <= 18:
-            print("Ele tem"{idade} "anos, o que faz dele menor de idade")
-
-        # criação componentes
-    input_nascimento = ft.TextField(label='Data',
-                                hint_text='Digite sua data de nascimento')
+    # criação componentes
+    input_nascimento = ft.TextField(label='Nascimento', hint_text='Digite seu ano de nascimento')
     btn_enviar = ft.FilledButton(
         text="Enviar",
         width=page.window.width,
@@ -31,6 +35,14 @@ def main(page: ft.Page):
     )
     txt_resultado = ft.Text(value="")
     # layout
-
+    page.add(
+        ft.Column(
+            [
+                input_nascimento,
+                btn_enviar,
+                txt_resultado,
+            ]
+        )
+    )
 
 ft.app(main)
